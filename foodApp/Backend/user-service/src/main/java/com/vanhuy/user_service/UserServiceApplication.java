58460887fdfbfceb2819e02640de458be3c9f80e
Abdul Rahman <abdulrahmanmemon159@gmail.com>
2025-08-28 12:53:16 +0530

    Abdul's Food App code

---
+++ foodApp/Backend/user-service/src/main/java/com/vanhuy/user_service/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.vanhuy.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+@EnableCaching
+public class UserServiceApplication  {
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+}
